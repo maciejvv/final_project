3fc9f17abc3c35d029787a7bd6b75c285ec1d686
maciejvv <61974597+maciejvv@users.noreply.github.com>
2020-09-24 13:27:01 +0200

    Add files via upload

---
+++ final_project_mw81779/src/pl/waw/sgh/mastermind/GameGUI.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh.mastermind;
+
+import javax.swing.*;
+
+public class GameGUI {
+    public JPanel main;
+    public JPanel newGuess;
+    public JPanel scoreboard;
+    public JTextField playersGuess;
+    public JButton confirmButton;
+    public JFormattedTextField tryOutOf;
+    public JFormattedTextField command;
+}
---
+++ final_project_mw81779/src/pl/waw/sgh/mastermind/PlayWithGUI.java
@@ -0,0 +1,7 @@
+package pl.waw.sgh.mastermind;
+
+public class PlayWithGUI {
+    public static void main(String[] args) {
+
+    }
+}
